7142054d6110bf227c489da770de20ce970ea044
Satory <kimkazakov42@gmail.com>
2024-10-31 20:53:02 +0300

    glfw

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,24 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+int g_windowSizeX = 640;
+int g_windowSizeY = 480;
+void gflwWindowsizecallback (GLFWwindow * window, int width, int height)
+{
+    g_windowSizeX = width;
+    g_windowSizeY = height;
+    glViewport (0,0, g_windowSizeX, g_windowSizeY);
+}
+
+void gflwKeycallback(GLFWwindow * window, int key, int scancode, int action, int mode)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, GL_TRUE);
+    }
+}
+
+
 int main(void)
 {
     GLFWwindow* window;
@@ -10,13 +28,15 @@ int main(void)
         return -1;
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(g_windowSizeX, g_windowSizeY, "Battle city", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
         return -1;
     }
 
+glfwSetWindowSizeCallback(window, gflwWindowsizecallback);
+// glfwSetKeyCallback(window, gflwKeycallback );
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
     if (!gladLoadGL())
